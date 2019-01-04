6416d35eae257a5e391c0b0d5a27a8e2b5323ecf
Cristian <chris.sedano.arenas@gmail.com>
2019-01-04 14:36:58 +0000

    Interface done
    Implement four Image View and one Button, and create two var imageViewDiceLeft and imageViewDiceRight, and IBAction func rollPressed

---
+++ Dices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dices
+//
+//  Created by Cristian Sedano Arenas on 3/1/19.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageViewDiceLeft: UIImageView!
+    @IBOutlet weak var imageViewDiceRight: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func rollPressed(_ sender: UIButton) {
+    }
+    
+}
+
