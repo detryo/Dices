4a1e9aafc986b26a37de2a9ec3a9c5f4478e7707
Cristian <chris.sedano.arenas@gmail.com>
2019-01-04 15:17:14 +0000

    Var, Let and Generate Numbers
    implement some var and let, and generate numbers code,

--- Dices/ViewController.swift
+++ Dices/ViewController.swift
@@ -13,13 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewDiceLeft: UIImageView!
     @IBOutlet weak var imageViewDiceRight: UIImageView!
     
-
+    var randomDiceIndexLeft : Int = 0
+    var randomDiceIndexRight : Int = 0
+    let diceImages : [String]
+    let nFaces : UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        diceImages = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+        nFaces = UInt32(diceImages.count)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        generateRandomDicde()
     }
 
     @IBAction func rollPressed(_ sender: UIButton) {
+        generateRandomDicde()
     }
     
+    func generateRandomDicde(){
+        
+        
+        randomDiceIndexLeft = Int(arc4random_uniform(nFaces))
+        randomDiceIndexRight = Int(arc4random_uniform(nFaces))
+    }
 }
 
